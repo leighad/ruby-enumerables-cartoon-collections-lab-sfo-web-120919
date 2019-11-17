ae8aae51dfb9e14c8eb600163246526fc15680a9
Leigha De La Rosa <cdversiontwo@gmail.com>
2019-11-17 03:21:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,11 +9,15 @@ def summon_captain_planet(array)
   array.map do
     |word| word.capitalize + "!"
   end
-  # new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.each do
+    long_words = []
+    |word| if word.length > 4
+    long_words << word
+  end
+  long_words
 end
 
 def find_the_cheese# code an argument here
