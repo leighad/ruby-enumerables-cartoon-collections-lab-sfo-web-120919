4a70c16cd912832f49fbb95b3d2a992d309da5ca
Leigha De La Rosa <cdversiontwo@gmail.com>
2019-11-17 03:11:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(array)
   # Your code here
   # array.each_with_index do 
-  array.with.each_index do 
+  array.each.with_index do 
     |name, index|
     puts "/#{index}. *#{name}/"
     end
