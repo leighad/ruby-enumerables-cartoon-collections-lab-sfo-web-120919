0e58ae72b50d43226559e18aa2ddd47df119af08
Leigha De La Rosa <cdversiontwo@gmail.com>
2019-11-17 03:12:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,8 @@
 def roll_call_dwarves(array)
-  # Your code here
-  # array.each_with_index do 
   array.each.with_index(1) do 
     |name, index|
     puts "/#{index}. *#{name}/"
     end
-
 end
 
 def summon_captain_planet# code an argument here
